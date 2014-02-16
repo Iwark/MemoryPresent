07d40196b611cd7fe1cb68276837b27a7dcfe164
Saiga <saigair19@gmail.com>
2014-02-16 11:22:39 +0900

    add edit movie class

---
+++ MemoryPresent/libs/EditMovie.h
@@ -0,0 +1,17 @@
+//
+//  EditMovie.h
+//  MemoryPresent
+//
+//  Created by InoueAyaka on 2014/02/16.
+//  Copyright (c) 2014年 Kohei Iwasaki. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+#import <AssetsLibrary/AssetsLibrary.h>
+#import <QuartzCore/QuartzCore.h>
+@interface EditMovie : NSObject
+@property (nonatomic,strong) AVAssetExportSession *assetExport;
+
+@end
